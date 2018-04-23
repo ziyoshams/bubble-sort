b3aaaca1abbe6a7bbac6a302d7cd38b9e3c722ad
Ziyovuddin <z.shamsiddini@gmail.com>
2018-04-23 14:11:29 -0400

    cleared comments

--- bubblesort.js
+++ bubblesort.js
@@ -1,20 +1,3 @@
-const swapCalls = {};
-// function bubbleSort(array) {
-//   let swapCounter;
-//   while (swapCounter !== 0) {
-//     swapCounter = 0;
-//     for (let i = 0; i < array.length - 1; i++) {
-//       if (array[i] > array[i + 1]) {
-//         let swappedVal = array[i];
-//         array[i] = array[i + 1];
-//         array[i + 1] = swappedVal;
-//         swapCounter++;
-//       }
-//     }
-//   }
-//   return array;
-// }
-
 Array.prototype.bubbleSort = function(callback) {
   let swapCounter;
   if (callback) {
@@ -39,7 +22,7 @@ Array.prototype.bubbleSort = function(callback) {
           this[i + 1] = swappedVal;
           swapCounter++;
         }
-      }
+      } 
     }
   }
   return this;
